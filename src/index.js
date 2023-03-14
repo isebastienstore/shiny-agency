e72f44ea4095edb6c65826fd136a6f92559730ff
iseb <sabastiandanbaibe@esp.sn>
2023-03-14 12:19:03 +0000

    unique sprint de conception d'une application react complète

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Home from './pages/Home';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+import {Routes, Route, BrowserRouter as Router} from 'react-router-dom'
+import Home from "./pages/Home";
+import Survey from "./pages/survey";
+import Header from "./components/Header";
+import Error from "./components/Error";
+import Results from "./pages/Results";
+import Freelances from "./pages/Freelances";
+import {SurveyProvider, ThemeProvider} from "./utils/context";
+import Footer from "./components/Footer";
+import GlobalStyle from "./utils/style/GlobalStyle";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
 root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+        <Router>
+            <ThemeProvider>
+                <SurveyProvider>
+                    <GlobalStyle/>
+                    <Header/>
+                    <Routes>
+                        <Route exact path="/" element={<Home />}/>
+                        <Route exact path="/survey/:questionNumber" element={<Survey />}/>
+                        <Route exact path="/results" element={<Results />}/>
+                        <Route exact path="/freelances" element={<Freelances />}/>
+                        <Route path="/*" element={<Error />}/>
+                    </Routes>
+                    <Footer/>
+                </SurveyProvider>
+            </ThemeProvider>
+        </Router>
+    </React.StrictMode>
+)
